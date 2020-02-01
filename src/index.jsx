6916515520d7db89330511ded93396233d6be44a
Bilal Itani <bilalitani1@gmail.com>
2020-02-01 01:43:27 -0500

    Set up react linked with webpack and babel; update index to render react

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const title = 'React with webpack and Babel';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
