7a1078824761a9ebae12eee0b5c7133f3e71e221
Bilal Itani <bilalitani1@gmail.com>
2020-02-04 00:22:28 -0500

    Add redux-saga to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
+import createSagaMiddleware from 'redux-saga'
 
-const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
 
 ReactDOM.render(
   <Provider store={store}>
