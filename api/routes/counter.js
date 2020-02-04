ed28a1e7bb1a3830cea2a7691404ed2d6461a107
Bilal Itani <bilalitani1@gmail.com>
2020-02-04 02:39:59 -0500

    Remove useless logs and comments from counter route

--- api/routes/counter.js
+++ api/routes/counter.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.patch('/', function(req, res, next) {
-  // res.send('respond with a resource');
   console.log(req.body);
-  console.log(res.body);
-  // res.json(JSON.parse(body)._links.self);
 });
 
 module.exports = router;
