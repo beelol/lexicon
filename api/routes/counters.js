4c2f6bd3a84d729046d4abec34aea3bdca8a53ed
Bilal Itani <bilalitani1@gmail.com>
2020-02-06 20:27:42 -0500

    Add functionality to find counter by id

--- api/routes/counters.js
+++ api/routes/counters.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models')
+var ObjectId = require('mongodb').ObjectID;
+
 
 /* GET users listing. */
 router.patch('/', function(req, res, next) {
   console.log(req.body);
 });
 
-router.get('/:id', function(req, res, next) {
-    res.json({counter: 0});
+router.get('/', async function(req, res, next) {
+  res.json({counters: await models.all.Counter.find()});
+});
+
+router.get('/:id', async function(req, res, next) {
+  var id = req.params.id;
+
+  var result = {count: 0}
+
+  await models.all.Counter.findOne({_id: ObjectId(id)}, function(err, foundCounter) {
+    if(!err && foundCounter !== null) result = foundCounter;
+  });
+
+  res.json(result);
 });
 
 
